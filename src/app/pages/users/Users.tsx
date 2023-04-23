aa1e955d23f53284df017197961b7ba51573ff1f
Trang Nguyen T <trangnguyent.@MBPM0039.local>
2023-04-23 21:13:50 +0700

    Create empty component

---
+++ src/app/pages/users/Users.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Users = () => {
+  return (
+    <div className="users-page">
+      <Outlet />
+    </div>
+  );
+};
+
+export default Users;
---
+++ src/app/pages/users/containers/UserDetail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserDetail = () => {
+  return <></>;
+};
+
+export default UserDetail;
